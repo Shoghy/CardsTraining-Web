a84c9e2e12a9ff185eb47d21d7e3aec1bfbdc2b3
Shoghy Martinez <shoghymartinez@gmail.com>
2024-03-05 08:16:39 -0400

    💄Creating a grid layout to organize the decks

--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,43 @@
 import { ScrollView, StyleSheet, View } from "react-native";
 
 export default function MainPage(){
+  const arr = new Array<number>(15).fill(0);
   return (
-    <View>
+    <View style={styles.backGroud}>
       <ScrollView
         style={styles.container}
         contentContainerStyle={styles.contentContainer}
       >
+        {arr.map((_, i) => <View key={i} style={styles.deck}/>)}
       </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  backGroud:{
+    flexDirection: "row",
+    height: "100%"
+  },
   container:{
     height: "80%",
     width: "100%",
-    marginVertical: "auto"
+    marginVertical: "auto",
+    backgroundColor: "#000",
+    padding: 10,
   },
   contentContainer:{
     flexDirection: "row",
-    flexWrap: "wrap"
+    flexWrap: "wrap",
+    justifyContent: "center",
+    gap: 10
+  },
+  deck:{
+    backgroundColor: "#f00",
+    flex: 1,
+    flexBasis: "35%",
+    aspectRatio: 1,
+    maxWidth: 200,
+    minWidth: 100
   }
 });
