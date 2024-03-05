cf9cfba1f2d877f168cd623156c6aed2766d6465
Shoghy Martinez <shoghymartinez@gmail.com>
2024-03-05 13:15:42 -0400

    🚨🎨 Creating a custom router
    Hago esto por el simple hecho de que no me autocompleta bien en el editor si uso la instancia original del objeto

---
+++ utils/custom_router.ts
@@ -0,0 +1,51 @@
+import { router } from "expo-router";
+
+export interface HrefObject{
+  /** Path representing the selected route `/[id]`. */
+  pathname?: string;
+  /** Query parameters for the path. */
+  params?: Record<string, number | string>;
+}
+
+export type Href = HrefObject | string;
+
+const custom_router = {
+  /** Go back in the history. */
+  back(){
+    router.back();
+  },
+  /** If there's history that supports invoking the dismiss and dismissAll function. */
+  canDismiss(){
+    return router.canDismiss();
+  },
+  /** If there's history that supports invoking the back function. */
+  canGoBack(){
+    return router.canGoBack();
+  },
+  /** Navigate to the provided href using a push operation if possible. */
+  dismiss(count?: number){
+    router.dismiss(count);
+  },
+  /** Navigate to first screen within the lowest stack */
+  dismissAll(){
+    router.dismissAll();
+  },
+  /** Navigate to the provided href. */
+  navigate(href: Href){
+    router.navigate(href);
+  },
+  /** Navigate to the provided href. */
+  push(href: Href){
+    router.push(href as never);
+  },
+  /** Navigate to route without appending to the history. */
+  replace(href: Href){
+    router.replace(href as never);
+  },
+  /** Update the current route query params. */
+  setParams(params: Record<string, number | string>){
+    router.setParams(params as never);
+  }
+};
+
+export default custom_router;
