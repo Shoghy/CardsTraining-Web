c6cbe05f910a4a9d8266386eeb8938c0b27830bc
Shoghy Martinez <shoghymartinez@gmail.com>
2024-03-11 10:59:44 -0400

    ⁉Huh?
    Función para contener componentes dentro de una variable.
    
    Dicha variable tiene acceso a las propiedades del componente y a funciones para cambiar las mismas.

---
+++ components/SelfComponent.ts
@@ -0,0 +1,58 @@
+import { useMemo, useState } from "react";
+
+export interface SelfComponentReturn<P>{
+  setProps: React.Dispatch<React.SetStateAction<P>>
+  props: Readonly<P>
+  Element(eProps: P): React.JSX.Element
+  SetProp<PN extends keyof P>(propName: PN, value: P[PN]): void
+  SetPropsAndMerge(props: P): void
+}
+
+export function SelfComponent<P, C extends (props: P) => React.JSX.Element>(
+  component: C,
+  props: P
+){
+  const _self: SelfComponentReturn<P> = useMemo(() => {
+    return {
+      props,
+      Element,
+      setProps: () => {},
+      SetProp: () => {},
+      SetPropsAndMerge: () => {}
+    };
+  }, []);
+
+  function Element(eProps: P){
+    const [props, setProps] = useState({
+      ...eProps,
+      ..._self.props
+    });
+    _self.props = props;
+    _self.setProps = setProps;
+    _self.SetProp = SetProp;
+    _self.SetPropsAndMerge = SetPropsAndMerge;
+    
+    function SetProp<PN extends keyof P>(
+      propName: PN,
+      value: P[PN]
+    ){
+      setProps((c) => {
+        c[propName] = value;
+        return {...c};
+      });
+    }
+
+    function SetPropsAndMerge(props: P){
+      setProps((c) => {
+        for(const propName in props){
+          c[propName] = props[propName];
+        }
+        return {... c};
+      });
+    }
+
+    return component(props);
+  }
+
+  return _self;
+}
