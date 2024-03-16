2559f40a3892f0dae4d61d9fc6b0844cf7d68b18
Shoghy Martinez <shoghymartinez@gmail.com>
2024-03-16 09:43:10 -0400

    ⁉Compatible with normal HTML Elements

---
+++ src/components/SelfComponent.tsx
@@ -0,0 +1,67 @@
+import { useMemo, useState } from "react";
+
+export interface SelfComponentReturn<P>{
+  setProps: React.Dispatch<React.SetStateAction<P>>
+  props: P
+  Element(eProps: P): React.JSX.Element
+  SetProp<PN extends keyof P>(propName: PN, value: P[PN]): void
+  SetPropsAndMerge(props: P): void
+}
+
+export function SelfComponent<
+  P extends React.JSX.IntrinsicElements[C],
+  C extends keyof React.JSX.IntrinsicElements
+>(component: C): SelfComponentReturn<P>;
+
+export function SelfComponent<
+  P extends object,
+  C extends (props: P) => React.JSX.Element
+>(component: C): SelfComponentReturn<P>;
+
+export function SelfComponent<P, C extends (props: P) => React.JSX.Element>(
+  Component: C
+){
+  const _self: SelfComponentReturn<P> = useMemo(() => {
+    function Element(eProps: P){
+      const [props, setProps] = useState({
+        ...eProps,
+        ..._self.props
+      });
+      _self.props = props;
+      _self.setProps = setProps;
+      _self.SetProp = SetProp;
+      _self.SetPropsAndMerge = SetPropsAndMerge;
+      
+      function SetProp<PN extends keyof P>(
+        propName: PN,
+        value: P[PN]
+      ){
+        setProps((c) => {
+          c[propName] = value;
+          return {...c};
+        });
+      }
+  
+      function SetPropsAndMerge(props: P){
+        setProps((c) => {
+          for(const propName in props){
+            c[propName] = props[propName];
+          }
+          return {... c};
+        });
+      }
+
+      return <Component {...props}/>;
+    }
+
+    return {
+      props: {} as never,
+      Element,
+      setProps: () => {},
+      SetProp: () => {},
+      SetPropsAndMerge: () => {}
+    };
+  }, []);
+
+  return _self;
+}
