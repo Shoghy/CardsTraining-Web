c6d84f9b7fa0efedf9dd7a15bac4479753c6912a
Shoghy Martinez <shoghymartinez@gmail.com>
2024-04-02 16:25:06 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 import babel from "vite-plugin-babel";
-import { chunkSplitPlugin } from "vite-plugin-chunk-split";
 
 // https://vitejs.dev/config/
 export default () => {
@@ -11,9 +10,6 @@ export default () => {
       react(),
       tsconfigPaths(),
       babel(),
-      chunkSplitPlugin({
-        strategy: "unbundle"
-      }),
     ]
   });
 };
