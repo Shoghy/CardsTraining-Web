a80ab4083891163ff28f772930a51033b888a2f7
Shoghy Martinez <shoghymartinez@gmail.com>
2024-04-03 10:36:43 -0400

    💄Creating styled button

---
+++ src/components/StyledButton.tsx
@@ -0,0 +1,19 @@
+import BasicButton, { BasicButtonProps } from "./BasicButton";
+import styles from "@/assets/css/components/StyledButton.module.css";
+
+export interface StyledButton extends BasicButtonProps{
+  look?: "green" | "blue" | "yellow"
+}
+
+export default function StyledButton({
+  className = "",
+  look = "blue",
+  ...prop
+}: StyledButton){
+  return (
+    <BasicButton
+      className={`${styles.btn} ${styles[look]} ${className}`}
+      {...prop}
+    />
+  );
+}
