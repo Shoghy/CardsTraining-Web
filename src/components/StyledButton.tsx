629819b8d282503a5215092a42ba24973c532d95
Shoghy Martinez <shoghymartinez@gmail.com>
2024-04-03 11:18:23 -0400

    💄Styled button new color added

--- src/components/StyledButton.tsx
+++ src/components/StyledButton.tsx
@@ -2,7 +2,7 @@ import BasicButton, { BasicButtonProps } from "./BasicButton";
 import styles from "@/assets/css/components/StyledButton.module.css";
 
 export interface StyledButton extends BasicButtonProps{
-  look?: "green" | "blue" | "yellow"
+  look?: "green" | "blue" | "yellow" | "red"
 }
 
 export default function StyledButton({
