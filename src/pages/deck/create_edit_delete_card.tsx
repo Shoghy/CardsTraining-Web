2b2c3f78694df681c11b456b5cbd52f7a4358508
Shoghy Martinez <shoghymartinez@gmail.com>
2024-04-04 10:40:13 -0400

    🎨Moving create_edit_delete_card logic
    I am not sure if this is better, but at least there is just UI in create_edit_delete_card

--- src/pages/deck/create_edit_delete_card.tsx
+++ src/pages/deck/create_edit_delete_card.tsx
@@ -1,181 +1,22 @@
-import { useEffect, useState } from "react";
 import styles from "@/assets/css/pages/create_card.module.css";
 import { faChevronCircleLeft, faFloppyDisk, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useNavigate, useParams } from "react-router-dom";
-import SelfAlert from "@/components/CustomAlert/SelfAlert";
-import { useDatabase } from "@/utils/AppContext";
-import CardModel from "@/model/CardModel";
-import DeckModel from "@/model/DeckModel";
-import { sleep } from "@/utils/functions";
 import StyledButton from "../../components/StyledButton";
-
-interface CardFillableData{
-  statement: string
-  answer: string
-  description: string
-  hint: string
-}
+import CreateEditDeleteState from "@/states/create_edit_delete_card_state";
 
 export default function CreateCard() {
-  const navigate = useNavigate();
-  const params = useParams();
-  const deckId = params.deckId as string;
-  const cardId = params.cardId;
-
-  const database = useDatabase();
-
-  const alert = SelfAlert();
-
-  const [statement, setStatement] = useState("");
-  const [answer, setAnswer] = useState("");
-  const [description, setDescription] = useState("");
-  const [hint, setHint] = useState("");
-
-  function EmptyFields(){
-    setStatement("");
-    setAnswer("");
-    setDescription("");
-    setHint("");
-  }
-
-  async function CreateCard(kargs: CardFillableData){
-    const deck = await database
-      .get<DeckModel>(DeckModel.table)
-      .find(deckId);
-
-    await database.write(async () => {
-      await database.get<CardModel>(CardModel.table)
-        .create((card) => {
-          card.statement = kargs.statement;
-          card.answer = kargs.answer;
-          card.description = kargs.description;
-          card.hint = kargs.hint ? kargs.hint : null;
-          card.lastTimePracticed = null;
-          card.score = 0;
-          card.timesRight = 0;
-          card.timesWrong = 0;
-          card.deck.set(deck);
-        });
-
-      await deck.update((changes) => {
-        changes.amountOfCards += 1;
-      });
-    });
-
-    EmptyFields();
-
-    alert.openWith({
-      title: "Success",
-      message: "Card added successfully",
-      xButton: () => {
-        alert.close();
-      },
-    });
-
-    await sleep(2500);
-
-    alert.close();
-  }
-
-  async function UpdateCard(kargs: CardFillableData){
-    if(!cardId){
-      throw new Error("You shouldn't see this");
-    }
-
-    const card = await database.get<CardModel>(CardModel.table).find(cardId);
-
-    await database.write(async () => {
-      card.update((card) => {
-        card.statement = kargs.statement;
-        card.answer = kargs.answer;
-        card.description = kargs.description;
-        card.hint = kargs.hint ? kargs.hint : null;
-      });
-    });
-
-    alert.openWith({
-      title: "Success",
-      message: "Card updated successfully",
-      xButton: () => {
-        alert.close();
-      },
-    });
-
-    await sleep(2500);
-
-    alert.close();
-  }
-
-  function SaveAction() {
-    const tStatement = statement.trim();
-    const tAnswer = answer.trim();
-    const tDescription = description.trim();
-    const tHint = hint.trim();
-
-    if (!tStatement || !tAnswer || !tDescription) {
-      alert.openWith({
-        title: "Error",
-        message: "You must fill the statement, answer and description fields.",
-        xButton: () => {
-          alert.close();
-        },
-      });
-      return;
-    }
-
-    if(!cardId){
-      CreateCard({
-        statement: tStatement,
-        answer: tAnswer,
-        description: tDescription,
-        hint: tHint,
-      });
-      return;
-    }
-    UpdateCard({
-      statement: tStatement,
-      answer: tAnswer,
-      description: tDescription,
-      hint: tHint,
-    });
-  }
-
-  async function DeleteCard(){
-    if(!cardId){
-      throw new Error("You shouldn't see this");
-    }
-
-    const card = await database.get<CardModel>(CardModel.table).find(cardId);
-    const deck = await card.deck;
-
-    await database.write(async () => {
-      await card.markAsDeleted();
-      await deck.update(deck => {
-        deck.amountOfCards -= 1;
-      });
-    });
-
-    alert.openWith({
-      title: "Success",
-      message: "Card deleted successfully",
-    });
-
-    await sleep(2500);
-
-    navigate(`/deck/${deckId}/manage-cards`);
-  }
-
-  useEffect(() => {
-    if(cardId === undefined) return;
-    (async () => {
-      const card = await database.get<CardModel>(CardModel.table).find(cardId);
-      setStatement(card.statement);
-      setAnswer(card.answer);
-      setDescription(card.description);
-      setHint(card.hint ?? "");
-    })();
-  }, []);
+  const manager = new CreateEditDeleteState();
+  const {
+    statement,
+    setStatement,
+    answer,
+    setAnswer,
+    description,
+    setDescription,
+    hint,
+    setHint,
+    alert,
+  } = manager;
 
   return (
     <div className={styles.backGround}>
@@ -231,7 +72,7 @@ export default function CreateCard() {
         <div className={styles.cardFooter}>
           <StyledButton
             look="yellow"
-            onClick={() => navigate(`/deck/${deckId}/manage-cards`)}
+            onClick={() => manager.navigate(`/deck/${manager.deckId}/manage-cards`)}
           >
             <FontAwesomeIcon
               icon={faChevronCircleLeft}
@@ -240,7 +81,7 @@ export default function CreateCard() {
             Back
           </StyledButton>
           {
-            cardId
+            manager.cardId
             &&
             <StyledButton
               look="red"
@@ -253,7 +94,7 @@ export default function CreateCard() {
                   },
                   buttons: [{
                     text: "Yes",
-                    onClick: DeleteCard
+                    onClick: () => manager.DeleteCard()
                   }]
                 })
               }
@@ -267,7 +108,7 @@ export default function CreateCard() {
           }
           <StyledButton
             look="green"
-            onClick={() => SaveAction()}
+            onClick={() => manager.SaveAction()}
           >
             <FontAwesomeIcon
               icon={faFloppyDisk}
---
+++ src/states/create_edit_delete_card_state.ts
@@ -0,0 +1,222 @@
+import SelfAlert, { SelfAlertObject } from "@/components/CustomAlert/SelfAlert";
+import CardModel from "@/model/CardModel";
+import DeckModel from "@/model/DeckModel";
+import { useDatabase } from "@/utils/AppContext";
+import { sleep } from "@/utils/functions";
+import { Database } from "@nozbe/watermelondb";
+import { useEffect, useState } from "react";
+import { NavigateFunction, Params, useNavigate, useParams } from "react-router-dom";
+
+export interface CardFillableData{
+  statement: string
+  answer: string
+  description: string
+  hint: string
+}
+
+export default class CreateEditDeleteState{
+  readonly statement: string;
+  readonly setStatement: React.Dispatch<React.SetStateAction<string>>;
+
+  readonly answer: string;
+  readonly setAnswer: React.Dispatch<React.SetStateAction<string>>;
+
+  readonly description: string;
+  readonly setDescription: React.Dispatch<React.SetStateAction<string>>;
+
+  readonly hint: string;
+  readonly setHint: React.Dispatch<React.SetStateAction<string>>;
+
+  readonly alert: SelfAlertObject;
+
+  readonly navigate: NavigateFunction;
+  readonly params: Readonly<Params<string>>;
+
+  readonly deckId: string;
+  readonly cardId: string | undefined;
+
+  readonly database: Database;
+
+  constructor(){
+    this.navigate = useNavigate();
+    this.params = useParams();
+    this.deckId = this.params.deckId as string;
+    this.cardId = this.params.cardId;
+
+    this.database = useDatabase();
+
+    const [statement, setStatement] = useState("");
+    this.statement = statement;
+    this.setStatement = setStatement;
+
+    const [answer, setAnswer] = useState("");
+    this.answer = answer;
+    this.setAnswer = setAnswer;
+
+    const [description, setDescription] = useState("");
+    this.description = description;
+    this.setDescription = setDescription;
+
+    const [hint, setHint] = useState("");
+    this.hint = hint;
+    this.setHint = setHint;
+
+    const alert = SelfAlert();
+    this.alert = alert;
+
+    useEffect(() => {
+      if(this.cardId === undefined) return;
+      const cardId = this.cardId;
+
+      (async () => {
+        const card = await this.database.get<CardModel>(CardModel.table).find(cardId);
+        setStatement(card.statement);
+        setAnswer(card.answer);
+        setDescription(card.description);
+        setHint(card.hint ?? "");
+      })();
+    }, []);
+  }
+
+  EmptyFields(){
+    this.setStatement("");
+    this.setAnswer("");
+    this.setDescription("");
+    this.setHint("");
+  }
+
+  async CreateCard(kargs: CardFillableData){
+    const {
+      database,
+      alert,
+    } = this;
+    const deck = await database
+      .get<DeckModel>(DeckModel.table)
+      .find(this.deckId);
+
+    await database.write(async () => {
+      await database.get<CardModel>(CardModel.table)
+        .create((card) => {
+          card.statement = kargs.statement;
+          card.answer = kargs.answer;
+          card.description = kargs.description;
+          card.hint = kargs.hint ? kargs.hint : null;
+          card.lastTimePracticed = null;
+          card.score = 0;
+          card.timesRight = 0;
+          card.timesWrong = 0;
+          card.deck.set(deck);
+        });
+
+      await deck.update((changes) => {
+        changes.amountOfCards += 1;
+      });
+    });
+
+    this.EmptyFields();
+
+    alert.openWith({
+      title: "Success",
+      message: "Card added successfully",
+      xButton: () => {
+        alert.close();
+      },
+    });
+
+    await sleep(2500);
+
+    alert.close();
+  }
+
+  async UpdateCard(kargs: CardFillableData){
+    const {
+      database,
+      alert,
+    } = this;
+    if(!this.cardId){
+      throw new Error("You shouldn't see this");
+    }
+
+    const card = await database.get<CardModel>(CardModel.table).find(this.cardId);
+
+    await database.write(async () => {
+      card.update((card) => {
+        card.statement = kargs.statement;
+        card.answer = kargs.answer;
+        card.description = kargs.description;
+        card.hint = kargs.hint ? kargs.hint : null;
+      });
+    });
+
+    alert.openWith({
+      title: "Success",
+      message: "Card updated successfully",
+      xButton: () => {
+        alert.close();
+      },
+    });
+
+    await sleep(2500);
+
+    alert.close();
+  }
+
+  SaveAction() {
+    const tStatement = this.statement.trim();
+    const tAnswer = this.answer.trim();
+    const tDescription = this.description.trim();
+    const tHint = this.hint.trim();
+
+    if (!tStatement || !tAnswer || !tDescription) {
+      this.alert.openWith({
+        title: "Error",
+        message: "You must fill the statement, answer and description fields.",
+        xButton: () => {
+          this.alert.close();
+        },
+      });
+      return;
+    }
+
+    if(!this.cardId){
+      this.CreateCard({
+        statement: tStatement,
+        answer: tAnswer,
+        description: tDescription,
+        hint: tHint,
+      });
+      return;
+    }
+    this.UpdateCard({
+      statement: tStatement,
+      answer: tAnswer,
+      description: tDescription,
+      hint: tHint,
+    });
+  }
+
+  async DeleteCard(){
+    if(!this.cardId){
+      throw new Error("You shouldn't see this");
+    }
+
+    const card = await this.database.get<CardModel>(CardModel.table).find(this.cardId);
+    const deck = await card.deck;
+
+    await this.database.write(async () => {
+      await card.markAsDeleted();
+      await deck.update(deck => {
+        deck.amountOfCards -= 1;
+      });
+    });
+
+    this.alert.openWith({
+      title: "Success",
+      message: "Card deleted successfully",
+    });
+
+    await sleep(2500);
+
+    this.navigate(`/deck/${this.deckId}/manage-cards`);
+  }
+}
