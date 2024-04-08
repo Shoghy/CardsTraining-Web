db091624e9233c52367c2f4fa8b35b246905bc53
Shoghy Martinez <shoghymartinez@gmail.com>
2024-04-08 11:53:25 -0400

    ⁉Updating EllipsisText
    The thing I did here, It is a crime? It works better, but I don't think is a good solution.

--- src/components/EllipsisText.tsx
+++ src/components/EllipsisText.tsx
@@ -1,61 +1,99 @@
-import { useEffect, useRef, useState } from "react";
-import styles from "@/assets/css/components/EllipsisText.module.css";
+import { useEffect, useRef } from "react";
 
 interface EllipsisTextProps extends React.HTMLAttributes<HTMLDivElement>{
   children?: string
 }
 
 export default function EllipsisText({
-  children, style,
+  children = "",
   ...props
 }: EllipsisTextProps){
-  const [text, setText] = useState<string | undefined>("\xa0");
   const divRef = useRef<HTMLDivElement>({} as never);
-  const lineHeight = useRef(0);
 
-  function CalculateLineClamp(){
+  function RemoveChildren(){
     const div = divRef.current;
-    div.style.webkitLineClamp = "unset";
+    if(div === null) return;
+    while(div.childNodes.length > 0){
+      div.childNodes[0].remove();
+    }
+  }
 
-    const parent = div.parentElement! as HTMLDivElement;
+  function AddEllipsis(){
+    const div = divRef.current;
+    let prevSpan = div.children[div.children.length-1] as HTMLSpanElement | null;
+    if(!prevSpan){
+      const span = document.createElement("span");
+      span.textContent = "...";
+      div.appendChild(span);
+      return;
+    }
+    const offsetTop = prevSpan.offsetTop;
+    div.appendChild(document.createElement("wbr"));
+    const span = document.createElement("span");
+    span.textContent = "...";
+    div.appendChild(span);
+    while(prevSpan && span.offsetTop > offsetTop){
+      prevSpan.remove();
+      prevSpan = div.children[div.children.length-3] as HTMLSpanElement | null;
+    }
+  }
 
-    const clampTo = Math.floor(parent.clientHeight/lineHeight.current);
-    div.style.webkitLineClamp = clampTo.toString();
+  function AddText(){
+    const div = divRef.current;
+    if(div === null) return;
+
+    RemoveChildren();
+
+    const testSpan = document.createElement("span");
+    testSpan.textContent = "A";
+    div.appendChild(testSpan);
+    const grothPerLine = parseFloat(window.getComputedStyle(div).height);
+
+    RemoveChildren();
+
+    let previousHeight = 0;
+    const units = Array.from(children);
+    let prevSpan: null | HTMLSpanElement = null;
+    for(let i = 0; i < units.length; ++i){
+      const span = document.createElement("span");
+      span.textContent = units[i];
+      div.appendChild(span);
+
+      const actualHeight = parseFloat(window.getComputedStyle(div).height);
+      const heightDiff = (actualHeight - previousHeight);
+
+      const grothDiff = Math.abs(heightDiff - grothPerLine);
+
+      previousHeight = actualHeight;
+      if(span.offsetTop === prevSpan?.offsetTop) continue;
+
+      if(heightDiff === 0 || grothDiff > 0.001){
+        span.remove();
+        AddEllipsis();
+        break;
+      }
+
+      prevSpan = span;
+    }
+    div.textContent = div.textContent as string;
   }
 
   useEffect(() => {
-    const parent = divRef.current.parentElement! as HTMLDivElement;
-
-    if(lineHeight.current === 0){
-      lineHeight.current = divRef.current.clientHeight;
-    }
-
-    const sizeObserver = new ResizeObserver(() => CalculateLineClamp());
-
-    sizeObserver.observe(parent);
-
-    setText(children);
-
-    //Waiting for the re-render
-    setTimeout(() => {
-      CalculateLineClamp();
-    }, 1);
+    const div = divRef.current;
+    const sizeObserver = new ResizeObserver(() => AddText());
+    sizeObserver.observe(div);
+    AddText();
     return () => {
       sizeObserver.disconnect();
+      RemoveChildren();
     };
   }, []);
 
   return (
     <div
+      ref={divRef}
       {...props}
-      style={{...{overflow: "hidden"}, ...style}}
     >
-      <div
-        ref={divRef}
-        className={styles.content}
-      >
-        {text}
-      </div>
     </div>
   );
 }
